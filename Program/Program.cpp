b23ba7a1e9f9cca8f77ac89df97bf01f322dc1f2
DongJuOK <kcary0907@gmail.com>
2024-10-18 21:44:32 +0900

    (2024.10.18) 얕은 복사와 깊은 복사

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,88 +1,102 @@
 ﻿#include <iostream>
 
-char character = 'C';
+using namespace std;
 
-void OnDamage(float damage = 0.0f)
+class Unit
 {
-	std::cout << damage << " 만큼 피해를 입었습니다." << std::endl;
-}
+private:
+	int health = 0;
+	int * count;
 
-// 기본 매개 변수를 지정하기 위해서는 오른쪽에서 부터 지정해주어야 합니다.
-void Coordinate(float x , float y, float z = 30)
-{
-	std::cout << "x의 값 : " << x << std::endl;
-	std::cout << "y의 값 : " << y << std::endl;
-	std::cout << "z의 값 : " << z << std::endl;
-}
+public:
+	Unit()
+	{
+		count = new int;
+
+		cout << "생성자" << endl;
+	}
+
+	Unit(Unit & clone)
+	{
+		// 얕은 복사는 객체가 메모리에서 해제될 때
+		// 생성자의 실행 순서와 반대로 소멸자가 실행되기
+		// 때문에 복사한 객체가 먼저 해제되므로 원래 있던
+        // 객체가 해제될 때 이미 해제된 메모리로 접근하게 되는 것 입니다.
+
+		count = clone.count;
+
+#pragma region 깊은 복사
+		// 객체를 복사할 때, 참조 값이 아닌 인스턴스 자체를
+		// 새로 복사하여 서로 다른 메모리를 생성하는 복사입니다.
+        count = new int;
+#pragma endregion
+
+		health = clone.health;
+
+		cout << "복사 생성자" << endl;
+	}
+
+	void SetData(int stamina)
+	{
+		health = stamina;
+	}
+
+	void GetData()
+	{
+		cout << "count : " << count << endl;
+		cout << "health : " << health << endl;
+	}
+
+	~Unit()
+	{
+		delete count;
+	}
+};
 
 int main()
 {
-#pragma region 스트림
-	// 시간의 흐름에 따라 연속적으로 발생하는 데이터의 흐름입니다.
+#pragma region 복사 생성자
+	// 같은 객체를 복사하여 생성시킬 때 호출되는
+	// 생성자입니다.
 
-	// int data = 0;
+	// Unit unit1;
 	// 
-	// std::cout << "data 변수의 값 입력 : ";
-	// 
-	// std::cin >> data;
-	// 
-	// std::cout << "data 변수의 값 : " << data << std::endl;
+	// Unit unit2(unit1);
 
-	// 스트림은 운영체제에 의해 형성되며, 스트림 자체에 버퍼라는
-	// 임시 메모리 공간이 존재합니다.
+	// 복사 생성자를 정의하지 않고, 객체를 복사하게 되면
+	// 기본 복사 생성자가 호출되기 때문에 얕은 복사로
+	// 객체가 복사됩니다.
+
 #pragma endregion
 
-#pragma region 동적 할당
-	// 프로그램을 실행 중에 필요한 만큼 메모리를
-	// 할당하는 작업입니다.
+#pragma region 얕은 복사
+	// 객체를 복사할 때 주소 값을 복사하여 같은
+	// 메모리 공간을 가리키게 하는 복사입니다.
 
-	//  int * ptr = new int;
-	//  int * dyanmicArray = new int[3];
-	//  
-	//  *ptr = 9999;
-	//  
-	//  dyanmicArray[0] = 10;
-	//  dyanmicArray[1] = 20;
-	//  dyanmicArray[2] = 30;
-	//  
-	//  for (int i = 0; i < 3; i++)
-	//  {
-	//  	std::cout << "dynamicArray[" << i << "]" << ":" << dyanmicArray[i] << std::endl;
-	//  }
-	//  
-	//  std::cout << "ptr이 가리키고 있는 메모리 값 : " << *ptr << std::endl;
-	//  std::cout << "ptr이 가리키고 있는 메모리 주소 : " << ptr << std::endl;
-	//  
-	//  delete [ ] dyanmicArray;
-	//  delete ptr;
-#pragma endregion
-
-#pragma region 기본 매개 변수
-	// 함수를 선언 및 정의할 때 사용자가 미리 기본값을
-	// 지정한 매개변수입니다.
-
-	// OnDamage();
-	// OnDamage(99);
-
-	// Coordinate(10, 20);
-	// Coordinate(5, 10, 15);
-#pragma endregion
-
-#pragma region 범위 지정 연산자
-	// 여러 범위에서 사용되는 식별자를 구분하는데 사용되는 연산자입니다.
-
-	// char character = 'A';
+	// int * ptr1 = new int;
 	// 
-	// std::cout << "지역 변수 character 변수의 값 : " << character << std::endl;
+	// int * ptr2 = ptr1;
 	// 
-	// // 범위 지정 연산자는 전역 변수와 같은 이름의 지역 변수가 선언되었을 때
-	// // 가장 가까운 범위에 선언된 변수의 이름을 사용하는 범위 규칙이
-	// // 존재하기 때문에 전역 변수가 호출되지 않습니다.
+	// *ptr1 = 99;
 	// 
-	// std::cout << "전역 변수 character 변수의 값 : " << ::character << std::endl;
+	// cout << "ptr1이 가리키는 값 : " << *ptr1 << endl;
+	// cout << "ptr2이 가리키는 값 : " << *ptr2 << endl;
+	// 
+	// delete ptr1;
 
-	// 범위 지정 연산자를 사용하게 되면 전역 변수의 범위를 지정하게 되므로
-	// 전역 변수에 접근할 수 있습니다.
+	// Unit marine1;
+	// 
+	// Unit marine2 = marine1;
+	// 
+	// marine2.SetData(60);
+	// 
+	// marine1.GetData();
+	// marine2.GetData();
+
+	// 얕은 복사의 경우 같은 객체가 서로 같은 메모리
+	// 공간을 참조하고 있기 때문에 하나의 객체로 값을
+	// 변경하게 되면 서로 참조된 객체도 함께 영향을 받습니다.
+
 #pragma endregion
 
 }

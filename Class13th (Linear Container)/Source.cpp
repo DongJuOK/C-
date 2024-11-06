cacc20a7120f6c6d52de159e8eb197fde415163e
DongJuOK <kcary0907@gmail.com>
2024-11-06 21:45:04 +0900

    (2024.11.06) 컨테이너 어댑터

---
+++ Class13th (Linear Container)/Source.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+#pragma region Vector 컨테이너
+	//  std::vector<int> vector;
+	//  
+	//  vector.reserve(6);
+	//  
+	//  vector.push_back(10); // [10]
+	//  vector.push_back(20); // [10][20]
+	//  vector.push_back(30); // [10][20][30]
+	//  vector.push_back(40); // [10][20][30][40]
+	//  vector.push_back(50); // [10][20][30][40][50]
+	//  
+	//  vector.pop_back(); // [10][20][30][40]
+	//  
+	//  vector.push_back(50); // [10][20][30][40][50]
+	//  vector.push_back(60); // [10][20][30][40][50][60]
+	//  vector.push_back(70); // [10][20][30][40][50][60][70]
+	//  
+	//  cout << "Capacity : " << vector.capacity() << endl;
+	//  cout << "size : " << vector.size() << endl;
+	//  
+	//  for (int i = 0; i < vector.size(); i++)
+	//  {
+	//  	cout << vector[i] << endl;
+	//  }
+#pragma endregion
+
+#pragma region List 컨테이너
+
+
+#pragma endregion
+
+
+	return 0;
+}
